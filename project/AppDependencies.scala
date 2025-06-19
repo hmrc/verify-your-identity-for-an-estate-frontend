2ae87407812c64f59ec722e73912ebe4e6931c0e
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2025-06-19 13:18:43 +0100

    DDCE-6670: Kosovo country added in both English and Welsh.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   private lazy val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.1.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % boostrapVersion
