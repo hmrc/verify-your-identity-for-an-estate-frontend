2773b82b18ab87701d8a747a949c565ca5d80bb2
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-10-24 11:34:31 +0100

    DDCE-6820 - pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,11 +4,11 @@ object AppDependencies {
 
   private val boostrapVersion = "9.19.0"
   
-  private val mongoVersion    = "2.7.0"
+  private val mongoVersion    = "2.10.0"
 
   private lazy val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.7.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.18.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % boostrapVersion
